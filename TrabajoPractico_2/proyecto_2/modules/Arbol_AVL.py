1b70e26a6bf74ac3681d0f2f57c80e6a4865db88
unknown <MasiSivila>
2025-06-02 22:56:11 -0300

    Organizacion de proyecto2 y cambios de ABB a AVL

---
+++ TrabajoPractico_2/proyecto_2/modules/Arbol_AVL.py
@@ -0,0 +1,195 @@
+from modules.NodoArbol import NodoArbol
+
+class AVL:
+    def __init__(self): #CREO UN ABB VACIO SIN RAIZ Y DE TAMAÑO 0
+        self.__raiz = None 
+        self.__tamano = 0
+    
+    def devolver_tamano(self):
+        return self.__tamano
+
+    @property
+    def raiz(self):
+        return self.__raiz
+    
+    @property
+    def tamano(self):
+        return self.__tamano
+
+    def altura(self,nodo):
+        return nodo.altura if nodo else 0
+    
+    def actualizar_altura(self,nodo):
+        nodo.altura = 1 + max(self.altura(nodo.hijoIzquierdo),self.altura(nodo.hijoDerecho))
+
+    def factor_balance(self, nodo):
+        return self.altura(nodo.hijoIzquierdo) - self.altura(nodo.hijoDerecho)
+
+    def _rotar_derecha(self, nodoDesbalanceado):
+        hijoIzquierdo = nodoDesbalanceado.hijoIzquierdo
+        subarbolDerechoDelHijo = hijoIzquierdo.hijoDerecho
+
+        # Rotar
+        hijoIzquierdo.hijoDerecho = nodoDesbalanceado
+        nodoDesbalanceado.hijoIzquierdo = subarbolDerechoDelHijo
+
+        # Actualizar alturas
+        nodoDesbalanceado.altura = 1 + max(self.altura(nodoDesbalanceado.hijoIzquierdo), self.altura(nodoDesbalanceado.hijoDerecho))
+        hijoIzquierdo.altura = 1 + max(self.altura(hijoIzquierdo.hijoIzquierdo), self.altura(hijoIzquierdo.hijoDerecho))
+
+        return hijoIzquierdo
+
+
+    def _rotar_izquierda(self, nodoDesbalanceado):
+        hijoDerecho = nodoDesbalanceado.hijoDerecho
+        subarbolIzquierdoDelHijo = hijoDerecho.hijoIzquierdo
+
+        # Rotar
+        hijoDerecho.hijoIzquierdo = nodoDesbalanceado
+        nodoDesbalanceado.hijoDerecho = subarbolIzquierdoDelHijo
+
+        # Actualizar alturas
+        nodoDesbalanceado.altura = 1 + max(self.altura(nodoDesbalanceado.hijoIzquierdo), self.altura(nodoDesbalanceado.hijoDerecho))
+        hijoDerecho.altura = 1 + max(self.altura(hijoDerecho.hijoIzquierdo), self.altura(hijoDerecho.hijoDerecho))
+
+        return hijoDerecho
+
+    def _factor_balance(self, nodo):
+        if not nodo:
+            return 0
+        return self.altura(nodo.hijoIzquierdo) - self.altura(nodo.hijoDerecho)
+    
+
+    def __len__(self): #Este metodo funciona para que puedas definir lafuncion len(arbol) y que funcione correctamente
+        """Devuelve el valor del tamaño actual del ABB"""
+        return self.__tamano 
+   
+    def __iter__(self):
+        return self.__raiz.__iter__() #Permite que el arbol pueda ser iterado por un for o por el operador "in" asi que es buena opción si se lo necesita en esos caso 
+    
+    def agregar(self, clave, valor):
+        if self.__raiz:#Pregunta si existe una raiz
+            self.__raiz = self._agregar(clave, valor, self.__raiz)#En caso que si llama al metodo auxiliar para que determine en donde se agrege el nuevo nodo
+        else: #Si no existe la raiz
+            self.__raiz = NodoArbol(clave, valor)#Determina un nodo arbol como una raiz
+        self.__tamano += 1 #Aumenta el tamaño
+
+
+    def eliminar(self,clave):
+        if self.__raiz:
+            self.__raiz = self._eliminar_recursivo(self.__raiz,clave)
+        else:
+            raise KeyError(f"Clave {clave} no se encuentra en el ABB.")
+            
+    def __contains__(self, clave): #METODO PARA QUE PYTHON IDENTIFIQUE EL OPERADOR IN PARA EL ABB AY QUE POR DEFECTO NO LO HACE
+        return self._buscar_recursivo(self.__raiz, clave) is not None
+
+    def _eliminar_recursivo(self, nodo, clave):
+        if nodo is None: #ESTA LINEA ES IMPORTANTE PORQUE O INDICA QUE EL NODO A ELIMINAR NO EXISTE EN EL ARBOL O QUE EL ARBOL ESTA VACIO, PARA EN AMBOS CASOS TERMINAR
+            if nodo is None:#SI EL NODO NO EXISTE
+                raise KeyError(f"Clave {clave} no se encuentra en el ABB.") 
+
+        if clave < nodo.clave: #SI LA CLAVE A ELIMINAR ES MENOR QUE LA CLAVE DEL NODO EN EL ARBOL
+            nodo.hijoIzquierdo = self._eliminar_recursivo(nodo.hijoIzquierdo, clave) #APLICA EL METODO RECURSIVAMENTE PARA EL HIJO IZQUIERDO(PARA SEGUIR BAJANDO POR EL ARBOL)
+        elif clave > nodo.clave:#SI LA CLAVE ES MAYOR 
+            nodo.hijoDerecho = self._eliminar_recursivo(nodo.hijoDerecho, clave)#APLICA EL METODO RECURSIVAMENTE PARA EL HIJO DERECHO(PARA SEGUIR BAJANDO POR EL ARBOL)
+        else:
+            #SI EL NODO NO TIENE HIJOS(NODO HOJA) SOLO SE ELIMINA EL NODO Y SE RESTA 1 EL TAMAÑO
+            if nodo.hijoIzquierdo is None and nodo.hijoDerecho is None:
+                self.tamano -= 1
+                return None
+            #SI EL NODO TIENE UN SOLO HIJO SE ELIMINA EL NODO Y SE RESTA UNO EL TAMAÑO
+            if nodo.hijoIzquierdo is None:
+                self.tamano -= 1
+                return nodo.hijoDerecho
+            elif nodo.hijoDerecho is None:
+                self.tamano -= 1
+                return nodo.hijoIzquierdo
+            # SI EL NODO POSEE HIJO IZQUIERDO E HIJO DERECHO
+            sucesor = self._minimo(nodo.hijoDerecho) #BUSCA EL NODO CON LA CLAVE MINIMA DEL SUBARBOL DERECHO DEL NODO, PARA REEMPLAZAR
+            nodo.clave = sucesor.clave #LE ASIGNA LA CLAVE DEL SUCESOR 
+            nodo.cargaUtil = sucesor.cargaUtil #LE ASIGNA LA CARGA UTIL
+            nodo.hijoDerecho = self._eliminar_recursivo(nodo.hijoDerecho, sucesor.clave) #LO INCERTA COMO NUEVO NODO PADRE Y REACOMODA EL ARBOL
+        return nodo
+
+    def _minimo(self, nodo):
+        while nodo.hijoIzquierdo is not None:
+            nodo = nodo.hijoIzquierdo
+        return nodo
+    
+    def buscar(self, clave):
+        return self._buscar_recursivo(self.raiz, clave)
+
+    def _buscar_recursivo(self, nodo, clave):
+        if nodo is None:
+            return None  #NO EXISTE LA CLAVE EN EL ARBOL
+
+        if clave < nodo.clave:
+            return self._buscar_recursivo(nodo.hijoIzquierdo, clave)#APLICA RECURSIVAMENTE PARA EL HIJO IZQUIERDO 
+        elif clave > nodo.clave:
+            return self._buscar_recursivo(nodo.hijoDerecho, clave)#APLICAR RECURSIVAMENTE PARA EL HIJO DERECHO
+        else:
+            return nodo  #DEVUELVE EL NODO QUE SE ENCONTRO 
+
+    def obtener(self, clave):
+        nodo = self._buscar_recursivo(self.raiz, clave)
+        if nodo is not None:
+            return nodo.cargaUtil
+        else:
+            raise KeyError(f"La clave {clave} no se encuentra en el ABB")
+
+    def _agregar(self, clave, valor, nodoActual):
+        if clave < nodoActual.clave:
+            if nodoActual.tieneHijoIzquierdo():
+                nodoActual.hijoIzquierdo = self._agregar(clave, valor, nodoActual.hijoIzquierdo)
+            else:
+                nodoActual.hijoIzquierdo = NodoArbol(clave, valor, padre=nodoActual)
+        else:
+            if nodoActual.tieneHijoDerecho():
+                nodoActual.hijoDerecho = self._agregar(clave, valor, nodoActual.hijoDerecho)
+            else:
+                nodoActual.hijoDerecho = NodoArbol(clave, valor, padre=nodoActual)
+
+        self.actualizar_altura(nodoActual)
+
+        fe = self.factor_balance(nodoActual)
+
+        # Rotaciones
+        # Izquierda-Izquierda
+        if fe > 1 and clave < nodoActual.hijoIzquierdo.clave:
+            return self._rotar_derecha(nodoActual)
+
+        # Derecha-Derecha
+        if fe < -1 and clave > nodoActual.hijoDerecho.clave:
+            return self._rotar_izquierda(nodoActual)
+
+        # Izquierda-Derecha
+        if fe > 1 and clave > nodoActual.hijoIzquierdo.clave:
+            nodoActual.hijoIzquierdo = self._rotar_izquierda(nodoActual.hijoIzquierdo)
+            return self._rotar_derecha(nodoActual)
+
+        # Derecha-Izquierda
+        if fe < -1 and clave < nodoActual.hijoDerecho.clave:
+            nodoActual.hijoDerecho = self._rotar_derecha(nodoActual.hijoDerecho)
+            return self._rotar_izquierda(nodoActual)
+
+        return nodoActual
+
+        # Asignación con corchetes: ABB[clave] = valor
+
+    def __setitem__(self, clave, valor):
+        self.agregar(clave, valor)
+
+    # Acceso con corchetes: valor = ABB[clave]
+    def __getitem__(self, clave):
+        nodo = self._buscar_recursivo(self.__raiz, clave)
+        if nodo is not None:
+            return nodo.cargaUtil
+        else:
+            raise KeyError(f"Clave {clave} no encontrada en el árbol.")
+
+    # Eliminación con corchetes: del ABB[clave]
+    def __delitem__(self, clave):
+        self.eliminar(clave)
+
+#ESTOS TRES ULTIMOS METODOS SON PARA QUE PYTHON PERMITA OPERAR CON CORCHETES COMO SI FUERA UNA LISTA EL ABB
--- TrabajoPractico_2/proyecto_2/modules/NodoArbol.py
+++ TrabajoPractico_2/proyecto_2/modules/NodoArbol.py
@@ -1,24 +1,64 @@
 class NodoArbol:
     def __init__(self, clave, valor, izquierdo=None, derecho=None, padre=None):
-        self.clave = clave #Le asigno una clave de referencia
-        self.cargaUtil = valor #Le proporciono la carga util(datos del nodo)
-        self.hijoIzquierdo = izquierdo #Determino un hijo izquierdo
-        self.hijoDerecho = derecho #Determino un hijo derecho
-        self.padre = padre	# opcional, pero útil si está presente <-- Nose a que se refiere
-        self.altura = 0
+        self.__clave = clave #Le asigno una clave de referencia
+        self.__cargaUtil = valor #Le proporciono la carga util(datos del nodo)
+        self.__hijoIzquierdo = izquierdo #Determino un hijo izquierdo
+        self.__hijoDerecho = derecho #Determino un hijo derecho
+        self.__padre = padre	# opcional, pero útil si está presente <-- Nose a que se refiere
+        self.__altura = 1
+
+    @property
+    def clave(self):
+        return self.__clave
+    
+    @property
+    def cargaUtil(self):
+        return self.__cargaUtil
+    
+    @property
+    def hijoIzquierdo(self):
+        return self.__hijoIzquierdo
+    
+    @property
+    def hijoDerecho(self):
+        return self.__hijoDerecho
+    
+    @property
+    def padre(self):
+        return self.__padre
+    
+    @property
+    def altura(self):
+        return self.__altura
+    
+    @hijoDerecho.setter
+    def hijoDerecho(self,valor):
+        self.__hijoDerecho = valor
+
+    @hijoIzquierdo.setter
+    def hijoIzquierdo(self,valor):
+        self.__hijoIzquierdo = valor
+
+    @padre.setter
+    def padre(self,valor):
+        self.__padre = valor
+
+    @altura.setter
+    def altura(self,valor):
+        self.__altura = valor   
 
     def tieneHijoIzquierdo(self):
-        return self.hijoIzquierdo is not None
+        return self.__hijoIzquierdo is not None
 
     def tieneHijoDerecho(self):
-        return self.hijoDerecho is not None
+        return self.__hijoDerecho is not None
 
     def esHoja(self):
-        return self.hijoIzquierdo is None and self.hijoDerecho is None
+        return self.__hijoIzquierdo is None and self.hijoDerecho is None
 
     def __iter__(self):
         if self.tieneHijoIzquierdo():
-            yield from self.hijoIzquierdo
+            yield from self.__hijoIzquierdo
         yield (self.clave, self.cargaUtil)  # clave y valor como tupla
         if self.tieneHijoDerecho():
-            yield from self.hijoDerecho         
+            yield from self.__hijoDerecho         
