748edb1557b30e5976916ce613932ba5bee2c08c
rociofrederich2 <rocio.frederich@ingenieria.uner.edu.ar>
2025-06-05 18:20:37 -0300

    Corección palomas mensajeras

--- TrabajoPractico_2/proyecto_3/modules/palomas_mensajeras.py
+++ TrabajoPractico_2/proyecto_3/modules/palomas_mensajeras.py
@@ -17,21 +17,40 @@ if __name__ == "__main__":
             origen, destino, distancia = partes
             G.agregarArista(origen, destino, int(distancia))
 
-
-    # Ejecutar Prim
+    
     origen = G.obtenerVertice("Peligros") 
     prim(G, origen)
 
-    # Mostrar resultados
-    print("\n Resultados obtenidos: \n")
+  
+    print("\n Lista de aldeas (orden alfabético):")
+    aldeas_ordenadas = sorted([v.id for v in G])
+    for nombre in aldeas_ordenadas:
+        print(f"- {nombre}")
+
+   
+    print("\n Comunicación por aldea:\n")
+
+   
+    arbol = {}
+    for v in G:
+        pred = v.obtenerPredecesor()
+        if pred:
+            arbol.setdefault(pred.id, []).append(v.id)
+
     total = 0
     for v in sorted(G, key=lambda x: x.id):
         pred = v.obtenerPredecesor()
         if pred:
-            print(f"{v.id} recibe de {pred.id}")
-            print(f"Distancia: {v.obtenerDistancia()} leguas\n")
+            print(f"{v.id} recibe el mensaje desde {pred.id}.")
             total += v.obtenerDistancia()
         elif v.obtenerDistancia() == 0:
-            print(f"{v.id} inicia el envío del mensaje.\n")
-    print(f"Distancia total recorrida por las palomas: {total} leguas\n")
+            print(f"{v.id} inicia el envío del mensaje.")
 
+        hijos = arbol.get(v.id, [])
+        if hijos:
+            print(f"Debe reenviar la noticia a: {', '.join(sorted(hijos))}\n")
+        else:
+            print("No reenvía el mensaje.\n")
+
+ 
+    print(f" Distancia total recorrida por las palomas: {total} leguas\n")
